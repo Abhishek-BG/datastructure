19c663470c72a810541e2b0d60b03993c4c280bd
codegeeksofficial <abhigowda0190@gmail.com>
2022-09-10 20:33:36 +0530

    update

--- java/Array/pair_with_a_given_sum_in_the_rotated_sorted_Array.java
+++ java/Array/pair_with_a_given_sum_in_the_rotated_sorted_Array.java
@@ -21,35 +21,25 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 class sumarray {
+
+    public static boolean findsum(int arr[], int key, int size, int index) {
+
+        int pos = 0;
+        if (size > index && pos != index) {
+            if (arr[index] + arr[pos] == key) {
+                return true;
+            }
+            return findsum(arr, key, size, index + 1);
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         // given array
         int arr[] = { 11, 15, 6, 8, 9, 10 };
         Scanner sc = new Scanner(System.in);
         int key = sc.nextInt();
-        int index = 0;
-
-        boolean value = false;
-        for (int i = 1; i < arr.length; i++) {
-            index = i;
-            int left = index - 1;
-            int right = index + 1;
-            while (left >= 0) {
-                if (arr[index] + arr[left] == key) {
-                    System.out.println(arr[index] + "+" + arr[left]);
-                    value = true;
-                }
-                left -= 1;
-
-            }
-            while (right < arr.length) {
-                if (arr[index] + arr[right] == key) {
-                    System.out.println(arr[index] + "+" + arr[right]);
-                    value = true;
-                }
-                right += 1;
-            }
-        }
-
-        System.out.println(value);
+        int size = arr.length;
+        System.out.println(findsum(arr, key, size, 1));
     }
 }
